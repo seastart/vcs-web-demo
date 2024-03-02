fafa659e6aed4e826f0805ce240c39d2a6cfd15d
liuflyj <794678914@qq.com>
2024-03-02 11:24:50 +0800

    配置全局路径env

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,20 @@ const { PersistGate } = require("redux-persist/integration/react");
 import "./App.css";
 import routes from "./routes";
 import { renderRoutes, RouteConfig } from "react-router-config";
+
 function App() {
   useEffect(() => {
+    console.log(process.env);
+    console.log(typeof process.env.REACT_APP_VCS_URL, "vcsUrl");
+    console.log(process.env.REACT_APP_CLIENT_ID, "clientId");
+    console.log(process.env.REACT_APP_CLIENT_SECRET, "clientSecret");
+
     let vcsClient = new (VCS as any).VCSClient(
-      "http://192.168.0.229:9001/vcs",
-      "0a16828823ce41c5ad040be3ed384c14",
-      "04ff1dae18ae4b1fb7057e239aa1ff03"
+      process.env.REACT_APP_VCS_URL,
+      process.env.REACT_APP_CLIENT_ID,
+      process.env.REACT_APP_CLIENT_SECRET
     );
+    console.log(vcsClient, "vcsClient");
     console.log((VCS as any).VERSION, "VERSION");
     store.dispatch(setVersion((VCS as any).VERSION));
     store.dispatch(setVCSClient(vcsClient));
